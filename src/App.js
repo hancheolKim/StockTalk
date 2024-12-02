4ee79ded8f194a7d763b5126f5ed62e5dcd17c98
김한철 <gkscjf137@naver.com>
2024-12-02 20:49:35 +0900

    추가직후 삭제 가능하게 변경

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ const App = () => {
   const [error, setError] = useState(""); // 에러 메시지 상태
 
   // 방명록 데이터 조회
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get(
-          "https://w5d72d166d2ed4d422a9bb5407dfb7fd8.apppaas.app/home"
-        ); // 방명록 데이터 가져오기
-        setUsers(response.data); // 방명록 사용자 목록 업데이트
-      } catch (error) {
-        console.error("방명록 데이터를 불러오는 데 문제가 발생했습니다.", error);
-        setError("방명록 데이터를 불러오는 데 문제가 발생했습니다.");
-      }
-    };
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get(
+        "https://w5d72d166d2ed4d422a9bb5407dfb7fd8.apppaas.app/home"
+      ); // 방명록 데이터 가져오기
+      setUsers(response.data); // 방명록 사용자 목록 업데이트
+    } catch (error) {
+      console.error("방명록 데이터를 불러오는 데 문제가 발생했습니다.", error);
+      setError("방명록 데이터를 불러오는 데 문제가 발생했습니다.");
+    }
+  };
 
-    fetchUsers();
+  useEffect(() => {
+    fetchUsers(); // 컴포넌트가 마운트될 때 사용자 목록을 불러옵니다.
   }, []);
 
   const handleSubmit = async (event) => {
@@ -48,12 +48,36 @@ const App = () => {
       setMessage(response.data); // 성공 메시지 또는 실패 메시지 출력
       setName(""); // 입력란 초기화
       setEmail(""); // 입력란 초기화
-      setUsers([...users, { name, email }]); // 새로운 사용자 방명록에 추가
+      fetchUsers(); // 사용자 추가 후, 최신 목록을 다시 불러오기
     } catch (error) {
       setMessage("데이터를 추가하는 데 문제가 발생했습니다.");
       console.error("방명록에 사용자 추가 중 오류 발생:", error);
     }
-};
+  };
+
+  // 사용자 삭제 함수
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.post(
+        "https://w5d72d166d2ed4d422a9bb5407dfb7fd8.apppaas.app/deleteUser",
+        null, // body를 빈 값으로 설정
+        {
+          params: {
+            id: id,
+          },
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          }
+        }
+      );
+
+      setMessage(response.data); // 성공 메시지 또는 실패 메시지 출력
+      fetchUsers(); // 사용자 삭제 후, 최신 목록을 다시 불러오기
+    } catch (error) {
+      setMessage("사용자 삭제에 문제가 발생했습니다.");
+      console.error("사용자 삭제 중 오류 발생:", error);
+    }
+  };
 
   return (
     <div>
@@ -65,7 +89,7 @@ const App = () => {
         {/* 방명록 조회 */}
         <h2>한철 사이트 방명록</h2>
         {error && <div className="error-message">{error}</div>}
-        
+
         <div className="guestbook">
           {users.length > 0 ? (
             <ul>
@@ -73,6 +97,7 @@ const App = () => {
                 <li key={index}>
                   <p>이름: {user.name}</p>
                   <p>이메일: {user.email}</p>
+                  <button onClick={() => handleDelete(user.id)}>X</button> {/* 삭제 버튼 */}
                 </li>
               ))}
             </ul>
